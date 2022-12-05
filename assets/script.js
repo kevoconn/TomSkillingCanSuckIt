094914fb41f7710ed14fa585e1bbbaa6102015cc
github_kevoconn <kcoconnor84@gmail.com>
2022-12-05 16:56:45 -0600

    added icons, changed formatting

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,9 @@ let cities = JSON.parse(localStorage.getItem("cities")) || [];
 //functions
 function init() {
   //grab last search results from local storage and place on left side of page
-  cities.forEach(city => {
-    document.querySelector(".pastSearch").innerHTML += `<button data-city="${city}">${city}</button>`
-  })
+  cities.forEach((city) => {
+    document.querySelector(".pastSearch").innerHTML += `<button data-city="${city}">${city}</button>`;
+  });
 }
 let weather = {
   fetchWeather: function (city) {
@@ -23,7 +23,7 @@ let weather = {
 
   displayWeather: function (data) {
     document.querySelector(".forecast").innerHTML = "";
-    console.log(data)
+    console.log(data);
     let name = data.city.name;
     let icon = data.list[0].weather[0].icon;
     let description = data.list[0].weather[0].description;
@@ -32,7 +32,7 @@ let weather = {
     let speed = data.list[0].wind.speed;
     console.log(humidity);
     cities.push(name);
-    localStorage.setItem("cities", JSON.stringify(cities))
+    localStorage.setItem("cities", JSON.stringify(cities));
 
     document.querySelector(".city").innerText = name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -42,13 +42,23 @@ let weather = {
     document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
     console.log(icon);
 
-    data.list.forEach(day => {
+    data.list.forEach((day) => {
+      
+      
       let midnight = day.dt_txt.split(" ")[1];
-      if(midnight === "00:00:00"){
-        document.querySelector(".forecast").innerHTML += `<div class="cardOne"><div> ${day.dt_txt} </div><div> ${day.main.temp} </div></div>`
+      if (midnight === "00:00:00") {
+        let date = dayjs(day.dt_txt).format("M/D/YYYY");
+        document.querySelector(".forecast").innerHTML += 
+        `<div class="cardOne">
+        <div>${date} </div>
+        <img src = "${"https://openweathermap.org/img/wn/" + icon + '.png'}">
+        <div>${day.main.temp + "°F"} </div>
+        <div>${day.weather[0].description}</div>
+        <div>${day.main.humidity + "%"}</div>
+        <div>${day.wind.speed + "mph"}</div></div>`;
+        
+        
       }
-      
-      
     });
   },
 
@@ -66,59 +76,10 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     weather.search();
   }
 });
+
 init();
-// // function requestCity() {
-//   // set assign variable to value of textbox on html page
-//   console.log();
-//   let city = "Chicago";
-//   console.log();
-//   let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=f30ccb78a681804fbfff4e5128eff799`;
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//       console.log(response);
-//     })
-//     .then(function (data) {
-//       // code goes here
-//       //chicago
-//       console.log(data);
-
-//       data.city.forEach((cityname) => {
-//         let titleEl = document.createElement("h3");
-//         titleEl.innerHTML = cityname.name;
-//       });
-//       data.list.forEach((weather) => {
-//         let listEl = document.createElement("li");
-//         listEl.innerHTML = weather.main;
-//         console.log(weather);
-//       });
-//       // chicago
-//       // temp, wind, and humidity goes here
-//     });
-// }
-
-//*function search() { //TODO:remove bracket
-//alert("hi");
-//let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Chicago&appid=517f19dc586407c39701b016a6edf914';
-//fetch(requestUrl)
-// .then(function (response) {
-//   return response.json();
-//  })
-// .then(function (data) {});
-//}
-//Code for chicago goes here ok?
-//chicago
-//temp wind humidity
-//5 day forecast
-
-//function calls
-// event listeners
-
-
-//searchBtn.addEventListener("click", searchCity);
-//search button event listener
-
-//click on past results search buttons
+document.querySelector(".pastSearch").addEventListener("click", cities);
+console.log(weather)
 
 //GIVEN a weather dashboard with form inputs
 //WHEN I search for a city
